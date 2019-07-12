82bc82d0c8d62a74a5af318230a3aa3e9dd6705f
Jared Farkas <jaredmfarkas@gmail.com>
2019-07-12 08:35:23 -0700

    Modify to progress towards adding state

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 function Keg(props) {
 
   return (
-    <div className = {props.sugarContent > 20 ? 'Sugary' : null}>
+    {props.availableKegs.map((keg, index) =>
+    <div className = {keg.sugarContent > 20 ? 'Sugary' : null}>
       <style jsx>{`
                 div {
                     background-color: orange;
@@ -45,22 +46,28 @@ function Keg(props) {
               }
             `}</style>
       <span className = {this.location.hash.includes('low') && props.pints > 10 ? 'Hidden' : null }>
-        <button className = {props.name}> Sell a pint</button>
+        <button className = {keg.name}> Sell a pint</button>
         <button className = '"edit " + {props.name}'> Edit Keg</button>
-        <h2>{props.brand} {props.name}</h2>
-        <h3> Price: ${props.price} | Grams of Sugar {props.sugarContent}</h3>
-        <h3 className = {props.pints < 10 ? 'Low' : null}>{props.pints} pints left</h3>
+        <h2>{props.brand} {keg.name}</h2>
+        <h3> Price: ${keg.price} | Grams of Sugar {props.sugarContent}</h3>
+        <h3 className = {keg.pints < 10 ? 'Low' : null}>{props.pints} pints left</h3>
       </span>
     </div>
+  )}
   );
 }
+
 Keg.propTypes = {
-  name: PropTypes.string.isRequired,
-  sugarContent: PropTypes.number.isRequired,
-  brand: PropTypes.string,
-  price: PropTypes.number.isRequired,
-  pints: PropTypes.number.isRequired
-};
+  availableKegs: PropTypes.array
+}
+//
+// Keg.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   sugarContent: PropTypes.number.isRequired,
+//   brand: PropTypes.string,
+//   price: PropTypes.number.isRequired,
+//   pints: PropTypes.number.isRequired
+// };
 
 
 
--- src/components/NewKegControl.jsx
+++ src/components/NewKegControl.jsx
@@ -8,6 +8,43 @@ class NewKegControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false
+      availableKegs = [
+        {
+          name: 'Water',
+          sugarContent: 0,
+          brand: 'Tap',
+          pints: 124,
+          price: 1
+        },
+        {
+          name: 'Orange Juice',
+          sugarContent: 5,
+          brand: 'Tropicana',
+          pints: 124,
+          price: 2
+        },
+        {
+          name: 'Cola',
+          sugarContent: 40,
+          brand: 'Coca-Cola',
+          pints: 124,
+          price: 3
+        },
+        {
+          name: 'Water',
+          sugarContent: 0,
+          brand: 'Dasani',
+          pints: 124,
+          price: 2
+        },
+        {
+          name: 'Cola',
+          sugarContent: 39,
+          brand: 'Pepsi',
+          pints: 4,
+          price: 3
+        }
+      ];
     };
     this.handleConfirmation = this.handleConfirmation.bind(this);
   }
@@ -17,16 +54,17 @@ class NewKegControl extends React.Component {
   }
 
   render(){
-    let currentlyVisibleContent = null;
-    if (this.state.formVisibleOnPage){
-      currentlyVisibleContent = <NewKeg />;
-    } else {
-      currentlyVisibleContent = <Confirmation onConfirmation={this.handleConfirmation}/>;
-    }
-    return (
-      <div>
-        {currentlyVisibleContent}
-      </div>
+    // let currentlyVisibleContent = null;
+    // if (this.state.formVisibleOnPage){
+    //   currentlyVisibleContent = <NewKeg />;
+    // } else {
+    //   currentlyVisibleContent = <Confirmation onConfirmation={this.handleConfirmation}/>;
+    // }
+    // return (
+    //   <div>
+    //     {currentlyVisibleContent}
+    //   </div>
+    <Keg availableKegs={this.state.availableKegs}/>
     );
   }
 }
