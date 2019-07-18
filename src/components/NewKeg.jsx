c785a869f20a3b49b60223ddfebea3a36ce406a3
Jared Farkas <jaredmfarkas@gmail.com>
2019-07-18 14:49:16 -0700

    Fix broken add/edit

--- src/components/NewKeg.jsx
+++ src/components/NewKeg.jsx
@@ -8,17 +8,17 @@ function NewKeg(props) {
   let _price = null;
   let _sugarContent = null;
   let _pints = null;
-  let drinkName = '';
-  let brandName = '';
-  let price = '';
-  let sugar = '';
-  if(window.location.href.split('/')[5] !== 'new'){
-    let loc = window.location.href.split('/')[5];
-    drinkName = props.availableKegs[loc].name;
-    price = props.availableKegs[loc].price;
-    sugar = props.availableKegs[loc].sugarContent;
-    brandName = props.availableKegs[loc].brand;
-  }
+  // let drinkName = '';
+  // let brandName = '';
+  // let price = '';
+  // let sugar = '';
+  // if(window.location.href.split('/')[5] !== 'new'){
+  //   let loc = window.location.href.split('/')[5];
+  //   drinkName = props.availableKegs[loc].name;
+  //   price = props.availableKegs[loc].price;
+  //   sugar = props.availableKegs[loc].sugarContent;
+  //   brandName = props.availableKegs[loc].brand;
+  // }
 
 
 
@@ -26,15 +26,11 @@ function NewKeg(props) {
 
   function handleNewKegSubmit(event) {
     event.preventDefault();
-    if(window.location.href.split('/').length > 5){
+    if(window.location.href.split('/')[5] != 'new'){
       props.editKeg({name: _name.value, brand: _brand.value, price: _price.value, sugarContent: _sugarContent.value, pints: _pints.value}, parseInt(window.location.href.split('/')[5]));
     }else{
       props.addKeg({name: _name.value, brand: _brand.value, price: _price.value, sugarContent: _sugarContent.value, pints: _pints.value});
     }
-    _name.value = '';
-    _brand.value = '';
-    _price.value = '';
-    _sugarContent.value ='';
   }
 
 
@@ -57,25 +53,21 @@ function NewKeg(props) {
           type='text'
           id='name'
           placeholder='Drink Name'
-          value = {drinkName}
           ref={(input) => {_name = input;}}/>
         <input
           type='text'
           id='brand'
           placeholder='Brand Name'
-          value={brandName}
           ref={(input) => {_brand = input;}}/>
         <input
           type ='number'
           id='price'
           placeholder='The Price per pint'
-          value = {price}
           ref={(input) => {_price = input;}}/>
         <input
           type ='number'
           id='sugarContent'
           placeholder='The sugar content per pint'
-          value = {sugar}
           ref={(input) => {_sugarContent = input;}}/>
         <input
           type ='hidden'
